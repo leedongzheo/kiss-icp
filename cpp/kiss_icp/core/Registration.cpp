7998e6d1ad61a0135388522d9391a348bc5b5201
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2025-09-04 22:41:59 +0700

    Update Registration.cpp

--- cpp/kiss_icp/core/Registration.cpp
+++ cpp/kiss_icp/core/Registration.cpp
@@ -36,8 +36,8 @@ struct HybridCorrespondence {
 };
 // ----------------------------------CACH 1--------------------------------------
 double ComputeAdaptivePlaharityThreshold(const std::vector<Eigen::Vector3d>& neighbors){
-    double base = 0.2;
-    double min_thr = 0.01, max_thr = 0.2;
+    double base = 0.07;
+    double min_thr = 0.001, max_thr = 0.2;
     double thr = base*20.0/std::max(20.0, static_cast<double>(neighbors.size()));
     return std::clamp(thr, min_thr, max_thr);
 }
